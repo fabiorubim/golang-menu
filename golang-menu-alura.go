04d808a6c38e4bfadb58d37079a180343be5ee44
esoft13 - Fabio Biasi Mello Rubim <fabiorubim@hotmail.com>
2020-02-10 00:48:26 -0300

    Using conditionals, IFs and switch..case.

--- golang-menu-alura.go
+++ golang-menu-alura.go
@@ -21,4 +21,64 @@ func main() {
 	fmt.Println("O ponteiro da minha variável comando é", &comando)
 	fmt.Println("O comando escolhido foi", comando)
 
+	//Go não utiliza os parenteses no IF
+	// if comando == 1 {
+	// 	fmt.Println("Monitorando...")
+	// } else if comando == 2 {
+	// 	fmt.Println("Exibindo logs...")
+	// } else if comando == 0 {
+	// 	fmt.Println("Saindo do programa")
+	// } else {
+	// 	fmt.Println("Não conheço este comando!")
+	// }
+
+	// ValorBoleano := false
+	// if ValorBoleano {
+	// 	fmt.Println("Este IF funciona!")
+	// }
+
+	// idade := 32
+	// expressaoBoleana := idade > 18
+	// if expressaoBoleana {
+	// 	fmt.Println("É maior de idade!")
+	// }
+
+	//Mais sucinto que utilizar IF
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+		break //Não é obrigatório, logo o Go não vai "reclamar"
+	case 2:
+		fmt.Println("Exibindo logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
+	// case 4: // Várias linhas dentro do case
+	// 	fmt.Println("Exemplo 4")
+	// 	fmt.Println("Outra linha")
+	// 	fmt.Println("Última linha")
+	default:
+		fmt.Println("Não conheço este comando!")
+	}
+
+	//É case sensitive. Precisa tratar como lowercase ou uppercase
+	// fmt.Println("Escolha um prato")
+	// fmt.Println("B - Batata frita")
+	// fmt.Println("P - Pizza")
+	// fmt.Println("H - Hambúrger")
+
+	// var prato string
+	// fmt.Scan(&prato)
+
+	// switch prato {
+	// case "B":
+	// 	fmt.Println("Você escolheu Batata frita!")
+	// case "P":
+	// 	fmt.Println("Você escolheu Pizza!")
+	// case "H":
+	// 	fmt.Println("Você escolheu hambúrguer!")
+	// default:
+	// 	fmt.Println("Prato não existe!")
+
+	// }
+
 }
